94517ab8384cbdbe5e803159693eab570d76b347
maxik001 <maxgusev@gmail.com>
2017-03-05 22:09:30 +0500

    .

---
+++ app/consts/consts_app.js
@@ -0,0 +1,11 @@
+import keyMirror from 'keymirror'
+
+export default keyMirror({
+  APP_ACCOUNT_LOGIN: null,
+  APP_ACCOUNT_LOGOUT: null,
+  APP_BOOTSTRAP_FAIL_API: null,
+  APP_BOOTSTRAP_FAIL_BROWSER: null,
+  APP_BOOTSTRAP_COMPLETED: null,
+  APP_BOOTSTRAP_IN_PROCESS: null,
+  APP_PAGE_NOT_FOUND: null
+})
